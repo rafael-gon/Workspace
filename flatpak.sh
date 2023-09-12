47d89b07f0a9545cda6386fc4a09091db6930f93
Rafael Goncalves <54370545+rafael-gon@users.noreply.github.com>
2023-09-12 12:09:14 -0300

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,19 +1,18 @@
-flatpak install flathub com.spotify.Client \
-                flathub com.stremio.Stremio \
-                flathub rest.insomnia.Insomnia \
-                flathub org.DolphinEmu.dolphin-emu \
-                flathub org.citra_emu.citra \
-                flathub org.ppsspp.PPSSPP \
-                flathub org.duckstation.DuckStation \
-                flathub com.valvesoftware.Steam \
-                flathub net.kuribo64.melonDS \
-                flathub org.yuzu_emu.yuzu \
-                flathub org.prismlauncher.PrismLauncher \
-                flathub net.pcsx2.PCSX2 \
-                flathub com.discordapp.Discord \
-                flathub org.qbittorrent.qBittorrent \
-                flathub com.dropbox.Client \
-                flathub com.getmailspring.Mailspring -y
+flatpak install flathub com.dropbox.Client /
+                flathub com.obsproject.Studio /
+                flathub com.stremio.Stremio /
+                flathub org.DolphinEmu.dolphin-emu /
+                flathub org.ppsspp.PPSSPP /
+                flathub org.duckstation.DuckStation /
+                flathub com.valvesoftware.Steam /
+                flathub org.citra_emu.citra /
+                flathub net.kuribo64.melonDS /
+                flathub com.heroicgameslauncher.hgl /
+                flathub org.yuzu_emu.yuzu /
+                flathub org.prismlauncher.PrismLauncher /
+                flathub net.pcsx2.PCSX2 /
+                flathub com.discordapp.Discord /
+                flathub org.qbittorrent.qBittorrent -y
 
 xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-bittorrent
 xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-torrent
