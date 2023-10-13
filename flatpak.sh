69251abc049a9c26d8e96820979c3da1514ddc69
Rafael Goncalves <54370545+rafael-gon@users.noreply.github.com>
2023-10-13 11:49:08 -0300

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,18 +1,4 @@
-flatpak install flathub com.dropbox.Client \
-                flathub com.obsproject.Studio \
-                flathub com.stremio.Stremio \
-                flathub org.DolphinEmu.dolphin-emu \
-                flathub org.ppsspp.PPSSPP \
-                flathub org.duckstation.DuckStation \
-                flathub com.valvesoftware.Steam \
-                flathub org.citra_emu.citra \
-                flathub net.kuribo64.melonDS \
-                flathub com.heroicgameslauncher.hgl \
-                flathub org.yuzu_emu.yuzu \
-                flathub org.prismlauncher.PrismLauncher \
-                flathub net.pcsx2.PCSX2 \
-                flathub com.discordapp.Discord \
-                flathub org.qbittorrent.qBittorrent -y
+flatpak install flathub com.stremio.Stremio -y
 
 xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-bittorrent
 xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-torrent
