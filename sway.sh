576bf862eb37e672f0ff5471b814c2121a3f9b42
Rafael Goncalves <54370545+rafael-gon@users.noreply.github.com>
2023-10-13 12:05:19 -0300

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -4,26 +4,6 @@ sudo timedatectl set-ntp true
 
 cd /tmp
 
-git clone https://aur.archlinux.org/swayfx-git.git
-cd swayfx-git
-makepkg -si --noconfirm
-
-cd /tmp
-
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
-
-cd /tmp
-
-git clone https://aur.archlinux.org/ly.git
-cd ly
-makepkg -si
-
-sudo systemctl enable ly.service
-
-cd /tmp
-
-git clone https://aur.archlinux.org/swaylock-effects.git
-cd swaylock-effects
-makepkg -si
