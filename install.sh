13b496bd8f7024c32a4dd8e43d8b830367035418
Rafael Goncalves <rafaelgon.tech@gmail.com>
2024-01-08 18:51:08 -0300

    Update Install script

--- install.sh
+++ install.sh
@@ -1,46 +1,33 @@
 #!/bin/bash
-whiptail --title "!!! WARNING !!!" --msgbox "To use this script you will need git and yay if you chose Arch based." 10 60
+whiptail --title "!!! WARNING !!!" --msgbox "To use this script you will need git and yay in your computer." 10 60
 
-geralPass=$(whiptail --title "Rafael Script" --passwordbox "Enter your password and choose Ok to continue." 10 60 3>&1 1>&2 2>&3)
+# - - - - - Geral - - - - - #
+yay -Syyuu
 
-#Password If
-geralPassExit=$?
-if [ $geralPassExit = 0 ]; then
+yay -S base-devel unzip unrar exa bat wget curl docker noto-fonts noto-fonts-cjk \
+       noto-fonts-emoji noto-fonts-extra bluez bluez-libs bluez-plugins bluez-utils \
+       flatpak asdf-vm nerd-fonts-git qbittorrent inkscape discord prismlauncher \
+       dropbox vlc visual-studio-code-bin insomnia nodejs-nativefier zsh starship \
+       alacritty gnome- wayland xorg-xwayland --noconfirm
 
-  # instalacao das coisas basicas que vai ter no pc independente do resto
+sudo systemctl enable bluetooth
 
-  sudo -S <<< $geralPass pacman -S git
+flatpak install flathub com.stremio.Stremio -y
 
-  cd /tmp
+xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-bittorrent
+xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-torrent
+xdg-mime default org.qbittorrent.qBittorrent.desktop x-scheme-handler/magnet
 
-  git clone https://aur.archlinux.org/yay.git
+mkdir -pv ~/.local/share/nativefier
 
-  cd yay
+cd ~/.local/share/nativefier
 
-  makepkg -si --noconfirm
+nativefier --name "WhatsApp" 'web.whatsapp.com'
+nativefier --name "Notion" 'https://www.notion.so/'
 
-  cd /home/rafael
+cd
 
-  yay -Syyuu base-devel unzip unrar exa bat wget curl ttf-fira-code docker noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra pulseaudio pulseaudio-bluetooth bluez bluez-libs bluez-plugins bluez-utils flatpak asdf-vm nerd-fonts-git qbittorrent inkscape discord pcsx2 prismlauncher yuzu melonds citra-git steam duckstation ppsspp dolphin-emu dropbox vlc visual-studio-code-bin insomnia android-studio nodejs-nativefier zsh starship alacritty --noconfirm
-
-  sudo -S <<< $geralPass systemctl enable bluetooth
-
-  flatpak install flathub com.stremio.Stremio -y
-  
-  xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-bittorrent
-  xdg-mime default org.qbittorrent.qBittorrent.desktop application/x-torrent
-  xdg-mime default org.qbittorrent.qBittorrent.desktop x-scheme-handler/magnet
-
-  mkdir -pv ~/.local/share/nativefier
-
-  cd ~/.local/share/nativefier
-
-  nativefier --name "WhatsApp" 'web.whatsapp.com'
-  nativefier --name "notion" 'https://www.notion.so/'
-
-  cd /home/rafael
-
-  echo "[Desktop Entry]
+echo "[Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
@@ -49,9 +36,9 @@ Name=WhatsApp
 Comment=Simple, secure, reliable messaging and calling
 Icon=whatsapp" >> ~/.local/share/applications/whatsapp.desktop
 
-  sudo -S <<< $geralPass chmod +x ~/.local/share/applications/whatsapp.desktop
+sudo chmod +x ~/.local/share/applications/whatsapp.desktop
 
-  echo "[Desktop Entry]
+echo "[Desktop Entry]
 Version=1.0
 Type=Application
 Terminal=false
@@ -60,53 +47,48 @@ Name=Notion
 Comment=Connected workspace where better, faster work happens
 Icon=notion" >> ~/.local/share/applications/notion.desktop
 
-  sudo -S <<< $geralPass chmod +x ~/.local/share/applications/notion.desktop
+sudo chmod +x ~/.local/share/applications/notion.desktop
 
-  displayServer=$(whiptail --title "Rafael Script" --menu "What desktop environment do you like to use?" 15 60 2 "1" "Budgie" "2" "Deepin" "3" "GNOME" "4" "Plasma" "5" "Pantheon" "6" "Hyprland" "7" "Sway" 3>&1 1>&2 2>&3)
+displayServer=$(whiptail --title "Rafael Script" --menu "What desktop environment do you like to use?" 15 60 3 "1" "Gnome" "2" "Plasma" "3" "Sway" 3>&1 1>&2 2>&3)
 
-  displayServerExit=$?
+displayServerExit=$?
 
-  if [ $displayServerExit = 0 ]; then
-    if [ $displayServer = 1 ]; then
-      # "---------- ---------- ---------- Budgie ---------- ---------- ----------" #
-      yay -S xorg budgie-desktop budgie-screensaver budgie-control-center budgie-desktop-view budgie-backgrounds network-manager-applet materia-gtk-theme papirus-icon-theme lightdm lightdm-slick-greeter lightdm-gtk-greeter --noconfirm
-      sudo -S <<< $geralPass systemctl enable lightdm
+if [ $displayServerExit = 0 ]; then
+  if [ $displayServer = 1 ]; then
+    # "---------- ---------- ---------- Gnome ---------- ---------- ----------" #
+    yay -S gnome-shell gnome-shell-extensions gdm gnome-backgrounds gnome-calculator gnome-calendar \
+          gnome-characters gnome-color-manager gnome-control-center gnome-font-viewer gnome-menus \
+          gnome-session gnome-settings-daemon gnome-software gnome-system-monitor gnome-text-editor \
+          loupe nautilus snapshot sushi xdg-desktop-portal-gnome xdg-user-dirs-gtk gnome-tweaks file-roller \
+          evince gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb  --noconfirm
 
-    elif [ $displayServer = 2 ]; then
-      # "---------- ---------- ---------- Deepin ---------- ---------- ----------" #
-      yay -S xorg deepin deepin-extra lightdm lightdm-deepin-greeter lightdm-gtk-greeter --noconfirm
-      sudo -S <<< $geralPass systemctl enable lightdm
+    xdg-mime query default inode/directory nautilus.desktop
 
-    elif [ $displayServer = 3 ]; then
-      # "---------- ---------- ---------- GNOME ---------- ---------- ----------" #
-      yay -S xorg gnome gnome-tweaks gdm --noconfirm
-      sudo -S <<< $geralPass systemctl enable gdm
+    sudo systemctl enable gdm
 
-    elif [ $displayServer = 4 ]; then
-      # "---------- ---------- ---------- Plasma ---------- ---------- ----------" #
-      yay -S xorg plasma dolphin latte-dock sddm okular ark spectacle gwenview kdeconnect libdbusmenu-glib packagekit-qt5 --noconfirm
-      sudo -S <<< $geralPass systemctl enable sddm
+  elif [ $displayServer = 2 ]; then
+    # "---------- ---------- ---------- Plasma ---------- ---------- ----------" #
+    yay -S swayfx-git swaylock-effects thunar thunar thunar-archive-plugin thunar-media-tags-plugin \
+          thunar-shares-plugin thunar-volman tumbler-extra-thumbnailers tumbler swaybg swaync-git \
+          pipewire wireplumber viewnior mousepad grim slurp xclip wl-clipboard light brightnessctl \
+          pamixer avizo ulauncher ly waybar --noconfirm
 
-    elif [ $displayServer = 5 ]; then
-      # "---------- ---------- ---------- Pantheon ---------- ---------- ----------" #
-      yay -S xorg lightdm lightdm-pantheon-greeter lightdm-gtk-greeter --noconfirm
-      sudo -S <<< $geralPass systemctl enable lightdm
+    xdg-mime query default inode/directory thunar.desktop
 
-    elif [ $displayServer = 6 ]; then
-      # "---------- ---------- ---------- Hyprland ---------- ---------- ----------" #
-      yay -S hyprland-git pipewire wireplumber xdg-desktop-portal-hyprland-git thunar thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler-extra-thumbnailers tumbler viewnior network-manager-applet blueman mousepad grim slurp xclip wl-clipboard light brightnessctl pamixer avizo ulauncher ly --noconfirm
-      sudo -S <<< $geralPass systemctl enable ly
-      sudo -S <<< $geralPass timedatectl set-ntp true
+    sudo systemctl enable ly
 
-    elif [ $displayServer = 7 ]; then
-      # "---------- ---------- ---------- Sway ---------- ---------- ----------" #
-      yay -S swayfx-git swaylock-effects thunar thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin thunar-volman tumbler-extra-thumbnailers tumbler network-manager-applet blueman swaybg swaync-git pipewire wireplumber viewnior mousepad grim slurp xclip wl-clipboard light brightnessctl pamixer avizo ulauncher ly --noconfirm
-      sudo -S <<< $geralPass systemctl enable ly
-      sudo -S <<< $geralPass timedatectl set-ntp true
+  elif [ $displayServer = 3 ]; then
+    # "---------- ---------- ---------- Sway ---------- ---------- ----------" #
+    yay -S plasma-desktop bluedevil discover dolphin latte-dock sddm okular ark spectacle gwenview \
+          kdeconnect flatpak-kcm kactivitymanagerd kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons \
+          kgamma khotkeys kinfocenter kmenuedit kpipewire kscreen kscreenlocker ksystemstats kwin kwrited \
+          layer-shell-qt libkscreen libksysguard milou plasma-browser-integration plasma-integration plasma-nm \
+          plasma-pa plasma-sdk plasma-systemmonitor plasma-thunderbolt plasma-vault plasma-workspace \
+          plasma-workspace-wallpapers plymouth-kcm polkit-kde-agent powerdevil sddm-kcm systemsettings \
+          xdg-desktop-portal-kde libdbusmenu-glib packagekit-qt5 --noconfirm
 
-    else
-      echo "bye bye 😙."
-    fi
+    sudo systemctl enable sddm
+
   else
     echo "bye bye 😙."
   fi
